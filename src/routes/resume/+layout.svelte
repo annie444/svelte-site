51a42574cc6870e9b1f92558ecfcd090a36a6b4d
Annie Ehler <annie.ehler.4@gmail.com>
2023-10-04 09:51:57 -0700

    Add analytics

--- src/routes/resume/+layout.svelte
+++ src/routes/resume/+layout.svelte
@@ -1,30 +1,33 @@
 <script lang="ts">
-  import '../../app.postcss';
-  import { onMount } from "svelte";
-  import { waitForElem } from "$lib";
-  import { blur } from "svelte/transition";
-  import { cubicIn } from "svelte/easing";
+	import '../../app.postcss';
+	import { onMount } from 'svelte';
+	import { waitForElem } from '$lib';
+	import { blur } from 'svelte/transition';
+	import { cubicIn } from 'svelte/easing';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 
-  let mounted = false;
-  onMount(async () => {
-    const e = await waitForElem("body#root");
-    if(!e) {
-      console.error("unable to find the body element");
-    } else {
-      const body = e as HTMLBodyElement;
-      const attr = body.getAttribute("data-theme");
-      if(!attr || attr !== "mono") {
-        console.log(attr);
-        body.setAttribute("data-theme", "mono");
-      }
-    }
-    mounted = true;
-  });
+	inject({ mode: dev ? 'development' : 'production' });
+
+	let mounted = false;
+	onMount(async () => {
+		const e = await waitForElem('body#root');
+		if (!e) {
+			console.error('unable to find the body element');
+		} else {
+			const body = e as HTMLBodyElement;
+			const attr = body.getAttribute('data-theme');
+			if (!attr || attr !== 'mono') {
+				console.log(attr);
+				body.setAttribute('data-theme', 'mono');
+			}
+		}
+		mounted = true;
+	});
 </script>
 
 {#if mounted}
-  <div in:blur={{ duration: 500, easing: cubicIn, opacity: 0.5 }}>
-    <slot />
-  </div>
+	<div in:blur={{ duration: 500, easing: cubicIn, opacity: 0.5 }}>
+		<slot />
+	</div>
 {/if}
-
