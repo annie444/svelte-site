80f6e01c24563dad19a6dcd7b42068be6c280ce3
Annie Ehler <annie.ehler.4@gmail.com>
2023-10-04 12:23:44 -0700

    Upgrade to supported versions

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.54c28ae3.js","_app/immutable/chunks/scheduler.21029ae3.js","_app/immutable/chunks/index.2a05704c.js","_app/immutable/chunks/singletons.4f9a04cc.js","_app/immutable/chunks/index.427e088d.js"];
+export const imports = ["_app/immutable/nodes/1.baca325c.js","_app/immutable/chunks/scheduler.21029ae3.js","_app/immutable/chunks/index.2a05704c.js","_app/immutable/chunks/singletons.eb1346c4.js","_app/immutable/chunks/index.427e088d.js"];
 export const stylesheets = [];
 export const fonts = [];
