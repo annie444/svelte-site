bb440716245a0d099c4564502be4a57b86ba4c1f
Annie Ehler <annie.ehler.4@gmail.com>
2023-10-04 18:29:32 -0700

    Add preprocessors

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,28 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte'],
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [vitePreprocess()],
+  preprocess: [
+    vitePreprocess(),
+    preprocess({
+      postcss: true,
+    })
+  ],
 
   vitePlugin: {
     inspector: true,
   },
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      runtime: 'nodejs18.x',
+      regions: ['sfo1'],
+      split: true
+    }),
     alias: {
       $lib: './src/lib',
       $components: './src/components',
